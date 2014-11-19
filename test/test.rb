432f28632dad07c37696284c0bdb9159250e1328
jose Antonio rodriguez leandro <alu0100696691@ull.edu.es>
2014-11-19 15:19:54 +0000

    test selenium home

--- test/test.rb
+++ test/test.rb
@@ -7,14 +7,12 @@ require 'sinatra'
 require 'selenium-webdriver'
 require 'rubygems'
 
-
 describe "test selenium pagina registro" do
-	before :each do
+	before :all do
 		@browser = Selenium::WebDriver.for :firefox
 		@url = 'localhost:4567/'
 		@browser.get(@url)		
 	end
-	
 	it "se abre pagina de Registro?" do
 		begin
 			element = @browser.find_element(:id,"login")
@@ -38,17 +36,12 @@ describe "test selenium pagina registro" do
 	end
 	
 	it "el nuevo usuario ya existe" do
-                @browser.find_element(:id,"nombre").send_keys("ana")
+		@browser.find_element(:id,"nombre").send_keys("ana")
                 begin
                         element = @browser.find_element(:id,"login")
-			#flash = @browser.find_element(:id,"aviso")
                 ensure
                         element.click
-			@browser.navigate.to(@url);
-			@browser.find_element(:id,"nombre").send_keys("ana")
-			@browser.find_element(:id,"login").click
-			flash = @browser.find_element(:id,"aviso")
-			flash = flash.text.to_s
+			flash = @browser.find_element(:id,"divAviso").find_element(:id,"aviso").text.to_s
 			assert_equal(false, flash.include?("Ese nombre ya existe, por favor, prueba con otro nombre."))
 			@browser.quit
                 end
@@ -56,4 +49,40 @@ describe "test selenium pagina registro" do
         end
 
 
-end	
+end
+
+describe "test selenium pagina home" do
+        before :all do
+                @browser = Selenium::WebDriver.for :firefox
+                @url = 'localhost:4567/'
+                @browser.get(@url)
+		@wait = Selenium::WebDriver::Wait.new(:timeout => 5)
+        end
+
+	after :all do
+		@browser.quit
+	end
+        it "se abre pagina de home?" do
+        	@browser.find_element(:id,"nombre").send_keys("luis")
+                begin
+                        element = @browser.find_element(:id,"login")
+                ensure
+                        element.click
+                        assert_equal("http://localhost:4567/index",@browser.current_url)
+                end
+	end
+
+	it "se actualiza el chat con nuevos comentarios?" do
+                begin
+                        @browser.find_element(:id,"nombre").send_keys("juan")
+			@browser.find_element(:id,"login").click
+			@wait.until { @browser.find_element(:id,"text").send_keys("hola") }
+			element = @wait.until { @browser.find_element(:id,"enviar") }
+			chat = @wait.until { @browser.find_element(:id,"chat") }
+                ensure
+                        element.click
+			chat = chat.text.to_s
+			assert_equal(false, chat.include?("juan : hola"))
+		end
+	end
+end
